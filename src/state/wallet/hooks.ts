b90569349e77e4473f64a4a311d62ebff1ee37de
Context Finance <github@context.finance>
2021-11-28 14:29:34 +0100

    chore: disable png

--- src/state/wallet/hooks.ts
+++ src/state/wallet/hooks.ts
@@ -9,7 +9,6 @@ import { isAddress } from '../../utils'
 import { useSingleContractMultipleData, useMultipleContractSingleData } from '../multicall/hooks'
 import { useTotalPngEarned } from '../stake/hooks'
 
-
 /**
  * Returns a map of the given addresses to their eventually consistent ETH balances.
  */
@@ -22,9 +21,9 @@ export function useETHBalances(
     () =>
       uncheckedAddresses
         ? uncheckedAddresses
-          .map(isAddress)
-          .filter((a): a is string => a !== false)
-          .sort()
+            .map(isAddress)
+            .filter((a): a is string => a !== false)
+            .sort()
         : [],
     [uncheckedAddresses]
   )
@@ -69,13 +68,13 @@ export function useTokenBalancesWithLoadingIndicator(
       () =>
         address && validatedTokens.length > 0
           ? validatedTokens.reduce<{ [tokenAddress: string]: TokenAmount | undefined }>((memo, token, i) => {
-            const value = balances?.[i]?.result?.[0]
-            const amount = value ? JSBI.BigInt(value.toString()) : undefined
-            if (amount) {
-              memo[token.address] = new TokenAmount(token, amount)
-            }
-            return memo
-          }, {})
+              const value = balances?.[i]?.result?.[0]
+              const amount = value ? JSBI.BigInt(value.toString()) : undefined
+              if (amount) {
+                memo[token.address] = new TokenAmount(token, amount)
+              }
+              return memo
+            }, {})
           : {},
       [address, validatedTokens, balances]
     ),
@@ -143,13 +142,9 @@ export function useAggregatePngBalance(): TokenAmount | undefined {
   const pngBalance: TokenAmount | undefined = useTokenBalance(account ?? undefined, png)
   const pngUnHarvested: TokenAmount | undefined = useTotalPngEarned()
 
-  if (!png) return undefined
+  return undefined
 
-  return new TokenAmount(
-    png,
-    JSBI.add(
-      pngBalance?.raw ?? JSBI.BigInt(0),
-      pngUnHarvested?.raw ?? JSBI.BigInt(0)
-    )
-  )
+  // if (!png) return undefined
+
+  // return new TokenAmount(png, JSBI.add(pngBalance?.raw ?? JSBI.BigInt(0), pngUnHarvested?.raw ?? JSBI.BigInt(0)))
 }
